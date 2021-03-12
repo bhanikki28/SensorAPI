d454375db746f3541e4840aae2db52b307c56587
JananiChandran <bharathy.poovalingam@gmail.com>
2021-03-13 10:44:25 +1100

    Code changes to have one or more filters through POST call

--- SensorDemo/src/main/java/com/nikki/controller/SensorReadController.java
+++ SensorDemo/src/main/java/com/nikki/controller/SensorReadController.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.function.Predicate;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
@@ -24,10 +22,8 @@ import com.nikki.model.DevicePredicates;
 
 @RestController
 public class SensorReadController {
-	
+
 	final static Logger logger = Logger.getLogger(SensorReadController.class.getName());
-	
-	
 
 	@Autowired
 	private DeviceDTO devDTO;
@@ -36,12 +32,9 @@ public class SensorReadController {
 	@RequestMapping("/get/device/id")
 	public List<Device> getDeviceListByName(@QueryParam("id") String device_id) {
 		logger.info("Getting Device by Id");
-		/*
-		List<Device> filteredDevList = devDTO.getDeviceList().stream().filter(DevicePredicates.getDevById(device_id))
-				.collect(Collectors.toList());*/
-		List<Device> filteredDevList = devDTO.getDeviceList().stream().filter(DevicePredicates.getPredicate("device_id",device_id))
-				.collect(Collectors.toList());
-		
+		List<Device> filteredDevList = devDTO.getDeviceList().stream()
+				.filter(DevicePredicates.getPredicate("device_id", device_id)).collect(Collectors.toList());
+
 		return filteredDevList;
 
 	}
@@ -59,60 +52,54 @@ public class SensorReadController {
 		return filteredDevList;
 
 	}
-	
+
 	/*
-	@RequestMapping("/get/devices")
-	public List<Device> getDeviceList() {
-		logger.info("Getting Device by Id and SensorName");
-		Map<String,String> filterMap = new HashMap<>();
-		filterMap.put("device_id","_AHU1_MAIN");
-		filterMap.put("sensor_name","FAN_STATUS");
-	    ArrayList<String> keyList = new ArrayList<>(filterMap.keySet());
-	    List<Device> tempList =devDTO.getDeviceList();
-		logger.info("Device List ModelSize"+tempList.size());
-
-		for(String filterKey:keyList) {
-			logger.info("Querying the filterlist based on key:"+filterKey);
-			logger.info("Querying the filterlist based on value:"+filterMap.get(filterKey));
-			
-			tempList = tempList.stream()
-					.filter(getPredicate(filterKey,filterMap.get(filterKey).toString()))
-					.collect(Collectors.toList());
-			logger.info("Temp List Size after applying filter{},size{}"+filterKey+":"+tempList.size());
-
-	    }
-		logger.info("Return List Size:"+tempList.size());
-		return tempList;
-
-	}*/
+	 * @RequestMapping("/get/devices") public List<Device> getDeviceList() {
+	 * logger.info("Getting Device by Id and SensorName"); Map<String,String>
+	 * filterMap = new HashMap<>(); filterMap.put("device_id","_AHU1_MAIN");
+	 * filterMap.put("sensor_name","FAN_STATUS"); ArrayList<String> keyList = new
+	 * ArrayList<>(filterMap.keySet()); List<Device> tempList
+	 * =devDTO.getDeviceList();
+	 * logger.info("Device List ModelSize"+tempList.size());
+	 * 
+	 * for(String filterKey:keyList) {
+	 * logger.info("Querying the filterlist based on key:"+filterKey);
+	 * logger.info("Querying the filterlist based on value:"+filterMap.get(filterKey
+	 * ));
+	 * 
+	 * tempList = tempList.stream()
+	 * .filter(getPredicate(filterKey,filterMap.get(filterKey).toString()))
+	 * .collect(Collectors.toList());
+	 * logger.info("Temp List Size after applying filter{},size{}"+filterKey+":"+
+	 * tempList.size());
+	 * 
+	 * } logger.info("Return List Size:"+tempList.size()); return tempList;
+	 * 
+	 * }
+	 */
 
 	@PostMapping(value = "/device/list", consumes = "application/json", produces = "application/json")
-	public List<Device>  showDevice(@RequestBody String filterString) {
-	    final JSONObject obj = new JSONObject(filterString);
-	    
-	    logger.info("JSON Object:"+obj);
-		Map<String,String> filterMap = new HashMap<>();
+	public List<Device> showDevice(@RequestBody String filterString) {
+		final JSONObject obj = new JSONObject(filterString);
+		Map<String, String> filterMap = new HashMap<>();
 		ArrayList<String> filterList = new ArrayList<>(obj.keySet());
-		for(String key:filterList) {
+		for (String key : filterList) {
 			filterMap.put(key, obj.get(key).toString());
-			logger.info("FilterKey:"+key);
 		}
 		ArrayList<String> keyList = new ArrayList<>(filterMap.keySet());
-	    List<Device> tempList =devDTO.getDeviceList();
-		logger.info("Device List ModelSize"+tempList.size());
+		List<Device> deviceList = devDTO.getDeviceList();
+		logger.info("Device List ModelSize" + deviceList.size());
 
-		for(String filterKey:keyList) {
-			logger.info("Querying the filterlist based on key:"+filterKey);
-			logger.info("Querying the filterlist based on value:"+filterMap.get(filterKey));
-			
-			tempList = tempList.stream()
-					.filter(DevicePredicates.getPredicate(filterKey,filterMap.get(filterKey).toString()))
+		for (String filterKey : keyList) {
+			logger.info("Querying the filterlist based on key:" + filterKey);
+			logger.info("Querying the filterlist based on value:" + filterMap.get(filterKey));
+
+			deviceList = deviceList.stream()
+					.filter(DevicePredicates.getPredicate(filterKey, filterMap.get(filterKey).toString()))
 					.collect(Collectors.toList());
-			logger.info("Temp List Size after applying filter{},size{}"+filterKey+":"+tempList.size());
-
-	    }
-		logger.info("Return List Size:"+tempList.size());
-		return tempList;
+			logger.info("Temp List Size after applying filter{},size{}" + filterKey + ":" + deviceList.size());
+		}
+		return deviceList;
 
 	}
 
